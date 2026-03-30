2197a52073df90501161b94870d7e1bccfc68eda
deekondajashwanth767587-commits <deekondajashwanth767587@gmail.com>
2026-03-30 20:25:01 +0530

    Time: 6 ms (61.84%) | Memory: 47.9 MB (65.79%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.java
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.java
@@ -2,10 +2,11 @@ class Solution {
     public boolean checkStrings(String s1, String s2) {
         int n = s1.length();
         
-        int[] even = new int[26]; 
-        int[] odd = new int[26];  
+        int[] even = new int[26]; // even indices
+        int[] odd = new int[26];  // odd indices
         
-         for (int i = 0; i < n; i++) {
+        // Count frequencies for s1
+        for (int i = 0; i < n; i++) {
             char c = s1.charAt(i);
             if (i % 2 == 0) {
                 even[c - 'a']++;
@@ -14,7 +15,8 @@ class Solution {
             }
         }
         
-         for (int i = 0; i < n; i++) {
+        // Subtract frequencies using s2
+        for (int i = 0; i < n; i++) {
             char c = s2.charAt(i);
             if (i % 2 == 0) {
                 even[c - 'a']--;
@@ -23,6 +25,7 @@ class Solution {
             }
         }
         
+        // Check if both arrays are zero
         for (int i = 0; i < 26; i++) {
             if (even[i] != 0 || odd[i] != 0) {
                 return false;
